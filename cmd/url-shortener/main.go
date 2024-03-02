e6e730b6a73b601fe8c838e7b1fe009e87ed6074
Nikita Bakunovich <112703570+kirshunya@users.noreply.github.com>
2024-03-02 16:33:31 +0300

    feat: Logger creating

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,14 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
-	//TODO logger : slog
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shorter", slog.String("env", cfg.Env))
+	log.Debug("debug url-shorter")
+
 	//TODO storage : postgresql
 	//TODO client : echo
 	//TODO run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
+	return log
 }
